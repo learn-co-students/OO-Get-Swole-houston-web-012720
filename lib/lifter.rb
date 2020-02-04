3a842d7aff1dd2a699d3f478d3764061d58e5abb
anamsoomro <anamsoomroed@gmail.com>
2020-02-04 10:27:24 -0600

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,41 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all 
+    @@all
+  end
+
+  def memberships 
+    Membership.all.select {|m| m.lifter == self}
+  end
+
+  def gyms 
+    memberships.map {|m| m.gym}
+  end
+
+  def self.avg_lift
+    lift_totals = self.all.map {|l| l.lift_total}
+    total = lift_totals.sum {|sum = 0, total| sum +=total}
+    avg = total / self.all.length
+  end
+
+  def membership_cost
+    memberships.sum {|total = 0, m| total += m.cost}
+  end
+
+  def sign_up (gym, cost)
+    Membership.new(self, gym, cost)
+  end
+
 end
+
+
+
--- lib/membership.rb
+++ lib/membership.rb
@@ -1,7 +1,17 @@
 class Membership
-  attr_reader :cost
+  attr_reader :cost, :gym, :lifter
 
-  def initialize(cost)
+  @@all = []
+
+  def initialize(lifter, gym, cost)
+    @gym = gym 
+    @lifter = lifter
     @cost = cost
+    @@all << self
   end
+
+  def self.all 
+    @@all
+  end
+  
 end
