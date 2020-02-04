37a2a4bab80d9c720e235a627a8c5cfadc399198
placidowang <placidow@gmail.com>
2020-02-04 10:04:36 -0600

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,36 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|membership| membership.lifter == self}
+  end
+
+  def gyms
+    memberships.map {|membership| membership.gym}
+  end
+
+  def self.average_lift_total
+    self.all.reduce(0) {|sum, lifter| sum + lifter.lift_total} / self.all.count
+  end
+
+  def total_membership_cost
+    memberships.reduce(0) {|sum, membership| sum + membership.cost}
+  end
+
+  def sign_new_membership(gym, cost)
+    Membership.new(cost, self, gym)
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,22 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+l1 = Lifter.new("Charlie", 405)
+l2 = Lifter.new("Brock", 315)
+l3 = Lifter.new("Arnold", 1450)
+l4 = Lifter.new("Thai", 20)
+
+g1 = Gym.new("Gold's")
+g2 = Gym.new("24 Hour Fitness")
+g3 = Gym.new("LA Fitness")
+
+m1 = Membership.new(30, l1, g2)
+m2 = Membership.new(30, l2, g2)
+m3 = Membership.new(35, l3, g1)
+m4 = Membership.new(30, l3, g2)
+m5 = Membership.new(45, l3, g3)
+m6 = Membership.new(45, l4, g3)
+
 binding.pry
 
 puts "Gains!"
