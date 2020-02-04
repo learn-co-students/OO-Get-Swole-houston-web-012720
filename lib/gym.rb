fc34f87b92287e18d22000e3b322f536e5618102
corn-cloud <comowaiye@gmail.com>
2020-02-04 10:32:50 -0600

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,33 @@
 class Gym
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|g| g.gym == self}
+  end 
+
+  def lifters
+    memberships.map {|g| g.lifter}
+  end 
+
+  def lifters
+    memberships.map {|g| g.lifter.name}
+  end 
+
+  def combined_lift_total
+    memberships.sum {|t| t.cost}
+  end
+
+  
+
+
 end
